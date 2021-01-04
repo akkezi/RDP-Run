425a6568d8bb2c86512ac8fbf3483bc48cd9e0f3
Akram A <akkezi@users.noreply.github.com>
2021-01-05 00:14:30 +0100

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 printf "Installing RDP Be Patience... " >&2
 {
-sudo useradd -m USER
-sudo adduser USER sudo
-echo 'USER:0123' | sudo chpasswd
+sudo useradd -m linux
+sudo adduser linux sudo
+echo 'linux:0123' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 sudo apt-get upgrade -y
@@ -13,13 +13,10 @@ sudo apt install --assume-yes --fix-broken
 sudo DEBIAN_FRONTEND=noninteractive \
 apt install --assume-yes xfce4 desktop-base
 
-sudo DEBIAN_FRONTEND=noninteractive \
-apt install --assume-yes desktop gnome-desktop
-
-sudo DEBIAN_FRONTEND=noninteractive \
-apt install --assume-yes laptop
-
-sudo systemctl set-default graphical.target
+sudo apt install gnome-session gdm3
+sudo apt install tasksel
+sudo apt install ubuntu-desktop #tasksel install ubuntu-desktop
+sudo service gdm3 start
 
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
 sudo apt install --assume-yes xscreensaver
@@ -28,13 +25,13 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano vim -y 
-sudo adduser USER chrome-remote-desktop
+sudo adduser linux chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
-su - USER -c """$CRP"""
+su - linux -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n\n'
 if sudo apt-get upgrade &> /dev/null
 then
