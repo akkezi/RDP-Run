ebb2b1302e9bc8157d8e692939fb8f2e8c8b66a9
Akram A <akkezi@users.noreply.github.com>
2021-01-04 23:13:42 +0100

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -1,16 +1,20 @@
 #! /bin/bash
 printf "Installing RDP Be Patience... " >&2
 {
-sudo useradd -m ALOK
-sudo adduser ALOK sudo
-echo 'ALOK:8426' | sudo chpasswd
+sudo useradd -m USER
+sudo adduser USER sudo
+echo 'USER:8426' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
+sudo apt-get upgrade
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo DEBIAN_FRONTEND=noninteractive \
-apt install --assume-yes xfce4 desktop-base
+#apt install --assume-yes xfce4 desktop-base
+sudo tasksel install desktop gnome-desktop
+sudo tasksel install laptop
+sudo systemctl set-default graphical.target
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
 sudo apt install --assume-yes xscreensaver
 sudo systemctl disable lightdm.service
