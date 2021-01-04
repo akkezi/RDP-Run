be472f2b745df8015de81911ad8bb05086d261ba
Akram A <akkezi@users.noreply.github.com>
2021-01-04 23:15:36 +0100

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -6,14 +6,14 @@ sudo adduser USER sudo
 echo 'USER:8426' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get upgrade -y
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo DEBIAN_FRONTEND=noninteractive \
 #apt install --assume-yes xfce4 desktop-base
-sudo tasksel install desktop gnome-desktop
-sudo tasksel install laptop
+sudo tasksel install desktop gnome-desktop -y
+sudo tasksel install laptop -y
 sudo systemctl set-default graphical.target
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
 sudo apt install --assume-yes xscreensaver
@@ -21,14 +21,14 @@ sudo systemctl disable lightdm.service
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
-sudo apt install nautilus nano -y 
-sudo adduser ALOK chrome-remote-desktop
+sudo apt install nautilus nano vim -y 
+sudo adduser USER chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
-su - ALOK -c """$CRP"""
+su - USER -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n\n'
 if sudo apt-get upgrade &> /dev/null
 then
